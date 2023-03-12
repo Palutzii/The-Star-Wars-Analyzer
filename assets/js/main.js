fbab9da51e7fa22c4fcb06f5cc2895ea034a83db
Pavel Sarmiento <pavelsarmiento@hotmail.com>
2023-03-12 20:35:55 +0100

    ADD: Import character class to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 import { Character } from "./components/character.js";
+
+async function getData(url){
+    let data = await fetch(url);
+    let json = await data.json();
+    return json;
+}
+
+let loadCharacters = async () => {
+    try {
+        let data = await getData("https://swapi.dev/api/people/");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+loadCharacters();
